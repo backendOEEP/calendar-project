85bc477b5db2dd41dab67f274e4017f7e1ccf734
Lee Min Woo <lmw7414@naver.com>
2022-10-05 23:36:14 +0900

    Initialize Project

---
+++ src/test/java/com/example/study/calendarproject/CalendarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study.calendarproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
