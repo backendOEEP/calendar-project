90c04881281ceb55e5be8aa34ab65873befa87de
Lee Min Woo <lmw7414@naver.com>
2022-10-17 02:02:39 +0900

    #7 - Data Rest 설정
    스케줄의 Json api를 자동으로 RESTful하게 만들게끔 설정
    
    Spring Data Rest 활용방법에 대해 참고해봄
    * https://blog.neonkid.xyz/m/221

--- src/main/java/com/example/study/calendarproject/repository/ScheduleRepository.java
+++ src/main/java/com/example/study/calendarproject/repository/ScheduleRepository.java
@@ -2,6 +2,8 @@ package com.example.study.calendarproject.repository;
 
 import com.example.study.calendarproject.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
