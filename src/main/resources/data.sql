f1cfc1c6c436846e59e0d36c4cc1627e3b61854e
Lee Min Woo <lmw7414@naver.com>
2022-11-09 20:38:42 +0900

    #7 - 테스트 데이터 추가
    data.sql에 스케줄 정보 1개를 추가하였다.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,7 +16,9 @@ insert into users(user_id, email, nickname, password) values
 insert into schedule(id, name, category, description, start_time, end_time, location, repeat_option) values
     (1, 'Backend Study', 'STUDY', 'backend study gogo', '2022-10-12 16:30:00', '2022-10-12 18:30:00', 'blindmelon', 'WEEK')
 ;
-
+insert into schedule(id, name, category, description, start_time, end_time, location, repeat_option) values
+    (2, 'Open Session', 'STUDY', '정기 스터디 모임 활동', '2022-11-20 16:30:00', '2022-11-20 19:30:00', '경대 북문 블라인드 멜론', 'WEEK')
+;
 -- plan 추가
 insert into plan(id, schedule_id, user_id) values
     (1, 1, 'a')
