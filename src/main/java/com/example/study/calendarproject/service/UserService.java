a228dcca4078443e5962995932fda79163a18d7f
parkjiyun <pjyun05200@gmail.com>
2022-11-23 14:19:18 +0900

    user crud 기능 추가

--- src/main/java/com/example/study/calendarproject/service/UserService.java
+++ src/main/java/com/example/study/calendarproject/service/UserService.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.file.attribute.UserPrincipal;
+
 @Slf4j
 @RequiredArgsConstructor
 @Transactional
@@ -15,5 +17,26 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    //userId로 user 정보 보기
+    public User getUser(String userId) {
+        return userRepository.findByUserId(userId);
+    }
+
+    //userId로 user 정보 수정하기
+    public User updateUser(String userId, User user) {
+        User tempUser = userRepository.findByUserId(userId);
+
+        tempUser.setPassword(user.getPassword());
+        tempUser.setUserName(user.getUserName());
+        tempUser.setEmail(user.getPassword());
+
+        User updatedUser = userRepository.save(tempUser);
+        return updatedUser;
+    }
+
+    //userId로 user 정보 삭제하기
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
 
 }
